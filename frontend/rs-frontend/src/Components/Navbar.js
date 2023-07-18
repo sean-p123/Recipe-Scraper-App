8310cca829050a8b5a995969a5598d105a54a7f9
Sean Patten <seanpatten84@gmail.com>
2023-07-18 18:10:22 +0100

    Frontend NavBar + Homepage

---
+++ frontend/rs-frontend/src/Components/Navbar.js
@@ -0,0 +1,26 @@
+import {Link, useMatch, useResolvedPath} from "react-router-dom";
+import {AiOutlinePlusCircle, AiOutlineSearch} from 'react-icons/ai';
+import {BsBook} from 'react-icons/bs'
+//AiOutlineSearch  AiOutlinePlusCircle
+export default function NavBar(){
+    
+    return (<nav className="nav">
+        <Link to="/" className="site-title">Recipe Keeper</Link>
+        <ul>
+            <CustomLink to="/lecturer"><AiOutlinePlusCircle size={36}/></CustomLink>
+            <CustomLink to="/student"><AiOutlineSearch size={36}/></CustomLink>
+            <CustomLink to="/student"><BsBook size={36}/></CustomLink>
+        </ul>
+    </nav>)
+}
+
+function CustomLink({to, children, ...props}){
+    
+    const resolvedPath = useResolvedPath(to)
+    const isActive = useMatch({path: resolvedPath.pathname, end:true});
+    return (
+        <li className={isActive ? "active": ""}>
+            <Link to={to}>{children}</Link>
+        </li>
+    )
+}
---
+++ frontend/rs-frontend/src/Components/Pages/home.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+export class Home extends React.Component{
+
+    
+    render(){
+        return(
+            <div>
+                <h1>Recipe Keeper</h1>
+                <div>
+                <label>Enter URL for recipe: </label>
+                <input type="text" id="recipeURL"></input>
+                <button onClick={this.handleButtonClick}>Load Recipe</button>
+                </div>
+                <iframe id="recipeFrame"  width="680" height="480" allowFullScreen></iframe>
+                <div>
+                <object data="https:/youtube.com" width="600" height="400">
+               
+                 Error: Embedded data could not be displayed.
+    </object>
+              </div>
+            </div>
+           
+            
+            );
+    }
+}
+export default Home;
