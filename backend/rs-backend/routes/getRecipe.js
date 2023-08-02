919dd1982115fa70e81a850ad1fa9d9b018b486b
Sean Patten <seanpatten84@gmail.com>
2023-08-02 17:16:00 +0100

    linked backend and frontend

--- backend/rs-backend/routes/getRecipe.js
+++ backend/rs-backend/routes/getRecipe.js
@@ -1,8 +1,48 @@
 var express = require("express");
 var router = express.Router();
+var cheerio = require("cheerio");
 
-router.get("/", function(req, res, next) {
+/*router.get("/", function(req, res, next) {
     res.send("API Working properly");
 });
 
+router.post("/load-recipe", function (req, res, next) {
+    // Extracting the URL data from the request body
+    const { url } = req.body;
+  
+    // Here you can perform web scraping or other operations based on the provided URL
+    // For this example, we'll just return the URL as is
+    const recipeURL = url;
+  
+    // Sending the recipe URL back to the frontend
+    res.json({ recipe: recipeURL });
+  });
+  */
+
+  let recipeURL =""
+  router.post("/", function (req, res, next) {
+    // Extracting the URL data from the request body
+    const { url } = req.body;
+  
+    // Here you can perform web scraping or other operations based on the provided URL
+    // For this example, we'll just return the URL as is
+   recipeURL = url;
+  
+    // Sending the recipe URL back to the frontend
+    res.json({ recipe: recipeURL });
+
+    console.log("nice " +recipeURL)
+    //now you have the url, you must scrape the website url using cheerio
+    //to get the information you want
+
+
+    
+  //  res.json({ message: "Recipe data received and processed successfully" });
+
+   // res.send(recipeURL);
+  });
+
+  router.get("/api/scrapedRecipe", function(req, res, next) {
+    res.json({recipe: recipeData});
+});
 module.exports = router;
--- frontend/rs-frontend/src/Components/Pages/home.js
+++ frontend/rs-frontend/src/Components/Pages/home.js
@@ -6,7 +6,26 @@ export class Home extends React.Component{
         const url = document.getElementById("recipeURL").value;
         const iframe = document.getElementById("recipeFrame");
 
-        iframe.src = url
+         // Sending the URL to the backend using fetch API
+    fetch("http://localhost:9000/getRecipe", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ url: url }),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          // Assuming the backend returns the recipe data in "data.recipe" property
+          iframe.src = data.recipe;
+
+        // iframe.src = url;
+        })
+        .catch((error) => {
+          console.error("Error loading recipe:", error);
+        });
+        console.log(url.recipe);
+        iframe.src = url;
     }
 
     render(){
@@ -21,7 +40,7 @@ export class Home extends React.Component{
                 <iframe id="recipeFrame"  width="680" height="480" allowFullScreen></iframe>
                 <div>
                 <object data="https:/youtube.com" width="600" height="400">
-               
+                    
                  Error: Embedded data could not be displayed.
     </object>
               </div>
