877e6d988c0e12f63f0ce873da35d7ce9c051119
Sean Patten <seanpatten84@gmail.com>
2023-08-16 20:09:35 +0100

    Recipe ingredient & instruction scraped and sent to get method

--- backend/rs-backend/routes/getRecipe.js
+++ backend/rs-backend/routes/getRecipe.js
@@ -19,58 +19,97 @@ router.post("/load-recipe", function (req, res, next) {
   });
   */
 
-  let recipeURL =""
-  router.post("/", async function (req, res, next) {
-    // Extracting the URL data from the request body
-    const { url } = req.body;
-  
-    // Here you can perform web scraping or other operations based on the provided URL
-    // For this example, we'll just return the URL as is
-   recipeURL = url;
-  
-    // Sending the recipe URL back to the frontend
-    res.json({ recipe: recipeURL });
-    console.log("nice " +recipeURL)
-    //now you have the url, you must scrape the website url using cheerio
-    //to get the information you want
+let recipeURL = "";
+let recipeIngredients = "";
+let recipeInstruction = "";
+let scriptContent;
+router.post("/", async function (req, res, next) {
+  // Extracting the URL data from the request body
+  const { url } = req.body;
 
-    const response = await axios.get(url);
-    const htmlContent = response.data;
+  // Here you can perform web scraping or other operations based on the provided URL
+  // For this example, we'll just return the URL as is
+  recipeURL = url;
 
-    //now use cheerio to parst the html data
-    const $ = cheerio.load(htmlContent);
+  // Sending the recipe URL back to the frontend
+  res.json({ recipe: recipeURL });
+  console.log("nice " + recipeURL)
+  //now you have the url, you must scrape the website url using cheerio
+  //to get the information you want
 
-    $('script').each((index, element) => {
-      const script = $(element);
+  const response = await axios.get(url);
+  const htmlContent = response.data;
 
-      if (script.attr('type') === 'application/ld+json') {
-        const scriptContent = script.html();
-        //parse script content to json
-        const scriptData = JSON.parse(scriptContent);
-        console.log("*** Start of script data ***");
-        console.log(scriptData);
-        console.log("*** End of script data ***");
+  //now use cheerio to parst the html data
+  const $ = cheerio.load(htmlContent);
 
-        if(scriptData.recipeIngredient !== undefined){
-          //access recipe ingredients
-          const recipeIngredients = scriptData.recipeIngredient;
+  //using this link https://tasty.co/recipe/french-onion-smash-burger-tacos
+  //possibly try to stop this loop after the first iteration
+  $('script').each((index, element) => {
+    const script = $(element);
 
-          console.log(recipeIngredients);
-          
-          recipeIngredients.forEach((ingredient, index) => {
-            console.log(`Ingredient ${index + 1}: ${ingredient}`);
-          });
-        }
+    if (script.attr('type') === 'application/ld+json') {
+      //const scriptContent = script.html();
+      scriptContent = script.html();
+      //parse script content to json
+      const scriptData = JSON.parse(scriptContent);
+     /* console.log("*** Start of script data ***");
+      console.log(scriptData);
+      console.log("*** End of script data ***");
+*/
+    /*  recipeIngredients = scriptData[0].recipeIngredient;
+      recipeInstruction = scriptData[0].recipeInstructions;
+      */
+      if (scriptData.recipeIngredient && scriptData.recipeInstructions) {
+      recipeIngredients = scriptData.recipeIngredient;
+      recipeInstruction = scriptData.recipeInstructions;
+
+      console.log("Recipe Ingredients:");
+      recipeIngredients.forEach((ingredient, index) => {
+        console.log(`${index + 1}. ${ingredient}`);
+      });
+
+      console.log("\nRecipe Instructions:");
+      recipeInstruction.forEach((instruction, index) => {
+        console.log(`${index + 1}. ${instruction.text}`);
+      });
+    }
+      //works for food network
+      /*
+      if(scriptData.recipeIngredient !== undefined){
+        //access recipe ingredients
+        recipeIngredients = scriptData.recipeIngredient;
+
+        console.log(recipeIngredients);
+
+        recipeIngredients.forEach((ingredient, index) => {
+          console.log(`Ingredient ${index + 1}: ${ingredient}`);
+        });
       }
-    })
+      else{
+        console.log("undefined");
+      }*/
 
-    
+      //    console.log($)
+    }
+  })
+
+  //  console.log(response.data);
+
   //  res.json({ message: "Recipe data received and processed successfully" });
 
-   // res.send(recipeURL);
-  });
+  // res.send(recipeURL);
+});
 
-  router.get("/api/scrapedRecipe", function(req, res, next) {
-    res.json({recipe: recipeData});
+router.get("/api/scrapedRecipe", function (req, res, next) {
+  const responseData = {
+    scriptContent: scriptContent,
+    recipeIngredients: recipeIngredients,
+    recipeInstruction: recipeInstruction
+  };
+  res.json(responseData);
+  /*res.json({ recipe: recipeIngredients });
+  res.json({ recipe: recipeInstruction});
+*/
 });
 module.exports = router;
